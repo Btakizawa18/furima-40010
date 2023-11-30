802fa70f7f79c1bf8c6a49eb0a6fcb9d6704ffb5
Btakizawa18 <akiduki.1329@gmail.com>
2023-11-30 21:02:55 +0900

    Rubocop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,7 +40,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id == current_user.id 
+    if @item.user_id == current_user.id
       @item.destroy
       redirect_to root_path
     else
