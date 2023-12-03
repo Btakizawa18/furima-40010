e722d9fe9135fc3ee359a6e21b80c3b3f8662b77
Btakizawa18 <akiduki.1329@gmail.com>
2023-12-03 12:49:26 +0900

    Formオブジェクトの作成

---
+++ app/models/order_form.rb
@@ -0,0 +1,24 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number
+
+  with_options presence: true do
+    # orderモデルのバリデーション
+    validates :user_id
+    validates :item_id
+    # paymentモデルのバリデーション
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :city
+    validates :block
+    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
+  end
+  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+
+  def save
+    # 商品購入情報を保存し、変数orderに代入する
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 住所を保存する
+    Payment.create(postcode: postcode, prefecture_id: prefecture_id, city: city, block: block, phone_number: phone_number, order_id: order.id)
+  end
+end
+
