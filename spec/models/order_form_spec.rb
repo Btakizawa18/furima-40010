38d3b37d0cb1d5425a279542598c531787ecd366
Btakizawa18 <akiduki.1329@gmail.com>
2023-12-05 16:08:04 +0900

    修正＋Rubocop

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
   before do
-    @order_form = FactoryBot.build(:order_form)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_form = FactoryBot.build(:order_form, user_id: @user.id, item_id: @item.id)
   end
 
   describe '配送先情報の保存' do
@@ -95,12 +97,17 @@ RSpec.describe OrderForm, type: :model do
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Phone number is invalid')
       end
-      it '電話番号が12桁以上あると保存できないこと' do
+      it '電話番号が12桁以上あると保存できない' do
         @order_form.phone_number = 123_456_789_101_112
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include('Phone number is invalid')
       end
-      it 'トークンが空だと保存できないこと' do
+      it '電話番号9桁以下では保存できない' do
+        @order_form.phone_number = '123456789'
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include('Phone number is invalid')
+      end
+      it 'トークンが空だと保存できない' do
         @order_form.token = nil
         @order_form.valid?
         expect(@order_form.errors.full_messages).to include("Token can't be blank")
